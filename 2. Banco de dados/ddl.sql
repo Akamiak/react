4c9e80f3cef83d375d516db8f4f78ce3abe0eba1
joao <jvmdsofical24@gmail.com>
2022-06-24 09:48:54 -0300

    ok

--- 2. Banco de dados/ddl.sql
+++ 2. Banco de dados/ddl.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE superHeroiDB;
-USE superHeroiDB;
+CREATE DATABASE superHeroisDB;
+USE superHeroisDB;
 
-CREATE TABLE tb_super_heroi (
+CREATE TABLE tb_super_herois (
 id_super_heroi			int primary key auto_increment,
 nm_super_heroi			varchar(100),
 ds_super_poder			varchar(200),
--- 2. Banco de dados/dml.sql
+++ 2. Banco de dados/dml.sql
@@ -1,10 +1,10 @@
-USE superHeroiDB;
+USE superHeroisDB;
 
-INSERT INTO tb_super_heroi(nm_super_heroi, ds_super_poder, bt_voa)
+INSERT INTO tb_super_herois(nm_super_heroi, ds_super_poder, bt_voa)
 		values ('Sasuke', 'Sharigan', false);
         
         
-select id_super_heroi id,
+select id_super_herois id,
 	   nm_super_heroi nome,
        ds_super_poder superPoder,
        bt_voa 		  voa
